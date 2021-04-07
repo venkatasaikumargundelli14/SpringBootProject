3033040baf8933d527a4ed4f19c636c295b0ca4b
venkatasai gundelli <gvs0595@gmail.com>
2021-04-07 11:07:28 +0530

    baisc auth

---
+++ src/main/java/com/venkatasai/restfulwebservices/basic/auth/AuthenticationBean.java
@@ -0,0 +1,28 @@
+package com.venkatasai.restfulwebservices.basic.auth;
+
+public class AuthenticationBean {
+	
+	private String message;
+
+	public AuthenticationBean(String message) {
+		this.message=message;
+	}
+
+	
+	public String getMessage() {
+		return message;
+	}
+
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	@Override
+	public String toString() {
+		return String.format("HelloWorld [message=%s]", message);
+	}
+	
+	
+
+}
